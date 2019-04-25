62612ae05dae5d13a3823ea960a14012730de092
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-04-25 15:09:26 +0900

    Add playerController

---
+++ Assets/pumpkin/Script/playerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    /// <summary>
+    /// オブジェクトの移動方向と速度
+    /// </summary>
+    private Vector2 speed;
+    /// <summary>
+    /// X方向の移動速度
+    /// </summary>
+    [SerializeField] private float speedX;
+    /// <summary>
+    /// Y方向の移動速度
+    /// </summary>
+    [SerializeField] private float speedY;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        gameObject.transform.Translate(Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="h">左右の入力情報</param>
+    /// <param name="v">上下の入力情報</param>
+    /// <returns></returns>
+    private Vector2 Move(float h,float v)
+    {
+        speed.x = h * speedX;
+        speed.y = v * speedY;
+        return speed;
+    }
+}
