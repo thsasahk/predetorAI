1a6c0e538afb4a42fcd29007b8a420d9d18acb12
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-04-28 03:43:02 +0900

    機能修正
    移動を1*1のマス目で管理

--- Assets/pumpkin/Script/playerController.cs
+++ Assets/pumpkin/Script/playerController.cs
@@ -5,38 +5,53 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     /// <summary>
-    /// オブジェクトの移動方向と速度
+    /// マス目のサイズ
     /// </summary>
-    private Vector2 speed;
+    [SerializeField] private float cellSize;
     /// <summary>
-    /// X方向の移動速度
+    /// 現在地点
     /// </summary>
-    [SerializeField] private float speedX;
+    private Vector2 current;
     /// <summary>
-    /// Y方向の移動速度
+    /// 目標地点
     /// </summary>
-    [SerializeField] private float speedY;
+    private Vector2 target;
+    /// <summary>
+    /// 移動速度
+    /// </summary>
+    [SerializeField] private float step;
 
     void Start()
     {
-        
+        target = gameObject.transform.position;//目標地点を現在地点に修正
     }
 
     void Update()
     {
-        gameObject.transform.Translate(Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+        current = gameObject.transform.position;//現在地点を記録
+        if (target == current)
+        {
+            SetTarget(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));//移動終了してから新たな目標地点を設定
+        }
+        transform.position = Vector3.MoveTowards(current, target, step * Time.deltaTime);//目標地点へ移動
     }
 
     /// <summary>
-    /// 
+    /// 上下左右の入力情報を符号化して目標地点を決定
     /// </summary>
     /// <param name="h">左右の入力情報</param>
     /// <param name="v">上下の入力情報</param>
     /// <returns></returns>
-    private Vector2 Move(float h,float v)
+    private Vector2 SetTarget(float h,float v)
     {
-        speed.x = h * speedX;
-        speed.y = v * speedY;
-        return speed * Time.deltaTime;
+        if (h != 0)
+        {
+            target.x += Mathf.Sign(h) * cellSize;
+        }
+        if (v != 0)
+        {
+            target.y += Mathf.Sign(v) * cellSize;
+        }
+        return target;
     }
 }
