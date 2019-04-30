d78a9113453886fc47be99fce497bd9f95f3e79d
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-04-30 14:45:26 +0900

    変更
    フォルダの構造を変更

---
+++ Assets/pumpkin/PredetorAssets/Script/GameDirector.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour
+{
+    /// <summary>
+    /// playerオブジェクトプレファブ
+    /// </summary>
+    [SerializeField]private GameObject playerPrefab;
+    /// <summary>
+    /// dragonオブジェクトプレファブ
+    /// </summary>
+    [SerializeField]private GameObject dragonPrefab;
+    /// <summary>
+    /// インスタンスしたplayerオブジェクト
+    /// </summary>
+    private GameObject player;
+    /// <summary>
+    /// インスタンスしたdragonオブジェクト
+    /// </summary>
+    private GameObject dragon;
+    /// <summary>
+    /// playerオブジェクトのポジション
+    /// </summary>
+    public Vector2 pPosition;
+    /// <summary>
+    /// dragonオブジェクトのポジション
+    /// </summary>
+    public Vector2 dPosition;
+    /// <summary>
+    /// dragonオブジェクトのスクリプト
+    /// </summary>
+    private dragonAI dragonAI;
+    /// <summary>
+    /// tileオブジェクト
+    /// </summary>
+    [SerializeField] private GameObject tile;
+
+    void Start()
+    {
+        player = Instantiate(playerPrefab);//playerPrefabをインスタンスしてplayerに格納
+        player.transform.position = pPosition;//インスタンスしたplayerオブジェクトのpositionを変更
+        dragon = Instantiate(dragonPrefab);//dragonPrefabをインスタンスしてdragonに格納
+        dragon.transform.position = dPosition;//インスタンスしたdragonオブジェクトのpositionを変更
+        dragonAI = dragon.GetComponent<dragonAI>();//インスタンスしたdragonオブジェクトのスクリプトを取得
+        Instantiate(tile, Vector2.zero, Quaternion.identity);//tileオブジェクトを生成
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dPosition = dragon.transform.position;//現在のdragonオブジェクトの位置を記録
+        pPosition = player.transform.position;//現在のplayerオブジェクトの位置を記録
+        dragonAI.pPosition = pPosition;//dragonAIに現在のplayerの位置を受け渡し
+    }
+}
---
+++ Assets/pumpkin/PredetorAssets/Script/playerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    /// <summary>
+    /// マス目のサイズ
+    /// </summary>
+    [SerializeField] private float cellSize;
+    /// <summary>
+    /// 現在地点
+    /// </summary>
+    private Vector2 current;
+    /// <summary>
+    /// 目標地点
+    /// </summary>
+    private Vector2 target;
+    /// <summary>
+    /// 移動速度
+    /// </summary>
+    [SerializeField] private float step;
+
+    void Start()
+    {
+        target = gameObject.transform.position;//目標地点を現在地点に修正
+    }
+
+    void Update()
+    {
+        current = gameObject.transform.position;//現在地点を記録
+        if (target == current)
+        {
+            SetTarget(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));//移動終了してから新たな目標地点を設定
+        }
+        transform.position = Vector3.MoveTowards(current, target, step * Time.deltaTime);//目標地点へ移動
+    }
+
+    /// <summary>
+    /// 上下左右の入力情報を符号化して目標地点を決定
+    /// </summary>
+    /// <param name="h">左右の入力情報</param>
+    /// <param name="v">上下の入力情報</param>
+    /// <returns></returns>
+    private Vector2 SetTarget(float h,float v)
+    {
+        if (h != 0)
+        {
+            target.x += Mathf.Sign(h) * cellSize;
+        }
+        if (v != 0)
+        {
+            target.y += Mathf.Sign(v) * cellSize;
+        }
+        return target;
+    }
+}
