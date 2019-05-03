0948314d7ce085957701900f22ff80693766118d
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-05-04 05:13:19 +0900

    変更
    update内でのnewの使用を回避出来るように記述

--- Assets/pumpkin/PhysicsAssets/script/batController.cs
+++ Assets/pumpkin/PhysicsAssets/script/batController.cs
@@ -25,7 +25,7 @@ public class batController : MonoBehaviour
     /// </summary>
     private Vector2 speed;
     /// <summary>
-    /// オブジェクトの角度
+    /// 進行方向の角度
     /// </summary>
     private float angle;
     /// <summary>
@@ -36,15 +36,13 @@ public class batController : MonoBehaviour
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
-        angle = transform.eulerAngles.z * (Mathf.PI / 180.0f);//自身の向いている方向角度をラジアン化、参考元→http://ftvoid.com/blog/post/631
-        //direction = Vector2.zero;
     }
 
     void Update()
     {
         speed = rb2D.velocity;//現在の速度を記録
         SetThruster(Input.GetAxis("Horizontal"));
-        Drive(Input.GetAxis("Vertical"));
+        Drive(Input.GetAxis("Vertical"), direction.normalized);
     }
 
     /// <summary>
@@ -53,17 +51,21 @@ public class batController : MonoBehaviour
     /// <param name="h">水平入力</param>
     private void SetThruster(float h)
     {
-        angle += h * thrusterPower * Time.deltaTime;
-        direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)).normalized;//自身の方向ベクトルを取得、正規化
+        angle = transform.eulerAngles.z * (Mathf.PI / 180.0f);//自身の向いている方向角度をラジアン化、参考元→http://ftvoid.com/blog/post/631
+        angle += h * thrusterPower * Time.deltaTime;//水平入力をもとに進行方向を変更
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, Mathf.Rad2Deg * angle);//オブジェクトに進行方向を向かせる
+        //direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)).normalized;//自身の方向ベクトルを取得、正規化
+        direction.x = Mathf.Cos(angle);//自身の方向ベクトルを取得
+        direction.y = Mathf.Sin(angle);//自身の方向ベクトルを取得
     }
 
     /// <summary>
     /// ベクトル方向に直進する
     /// </summary>
     /// <param name="v">垂直方向の入力</param>
-    private void Drive(float v)
+    private void Drive(float v,Vector2 d)
     {
-        rb2D.AddForce(v * power * direction * Time.deltaTime);//directionベクトル方向に力を加える
-        rb2D.AddForce(backPower * (v * direction - speed) * Time.deltaTime);//現在の進行方向と逆方向に力を加える、速度が大きいほど逆噴射も大きくなる。参考元→http://nnana-gamedev.hatenablog.com/entry/2017/09/07/012721
+        rb2D.AddForce(v * power * d * Time.deltaTime);//directionベクトル方向に力を加える
+        rb2D.AddForce(backPower * (v * d - speed) * Time.deltaTime);//現在の進行方向と逆方向に力を加える、速度が大きいほど逆噴射も大きくなる。参考元→http://nnana-gamedev.hatenablog.com/entry/2017/09/07/012721
     }
 }
