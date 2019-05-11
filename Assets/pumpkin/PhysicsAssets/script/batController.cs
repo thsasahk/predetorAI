7488b28dd3bc3a2b982c721235f1d84f0d578e4d
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-05-12 02:16:22 +0900

    機能変更
    タッチやクリックの座標を移動目標に設定、接近したらブレーキをかける

--- Assets/pumpkin/PhysicsAssets/script/batController.cs
+++ Assets/pumpkin/PhysicsAssets/script/batController.cs
@@ -32,39 +32,81 @@ public class batController : MonoBehaviour
     /// 角度の変化値
     /// </summary>
     [SerializeField] private float thrusterPower;
+    /// <summary>
+    /// タッチ座標
+    /// </summary>
+    public Vector2 touch;
+    /// <summary>
+    /// 現在値を記録
+    /// </summary>
+    private Vector2 bPosition;
+    /// <summary>
+    /// オブジェクトから目標へのベクトル
+    /// </summary>
+    private Vector2 target;
+    /// <summary>
+    /// 現在の進路ベクトルと目標へのベクトルの角度差
+    /// </summary>
+    private float targetAngle;
+    /// <summary>
+    /// 進路ベクトルと目標ベクトルの外積
+    /// </summary>
+    private Vector3 cross;
+    /// <summary>
+    /// 進路の修正値
+    /// </summary>
+    private float addAngle;
+    /// <summary>
+    /// ブレーキをかけるタイミング
+    /// </summary>
+    [SerializeField] private float brake;
 
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        touch = gameObject.transform.position;//初期化
     }
 
     void Update()
     {
         speed = rb2D.velocity;//現在の速度を記録
-        SetThruster(Input.GetAxis("Horizontal"));
-        Drive(Input.GetAxis("Vertical"), direction.normalized);
-    }
-
-    /// <summary>
-    /// 進行方向を変更する
-    /// </summary>
-    /// <param name="h">水平入力</param>
-    private void SetThruster(float h)
-    {
+        bPosition = gameObject.transform.position;//現在の位置を記録
+        target = touch - bPosition;//目標地点への方向ベクトルを取得
         angle = transform.eulerAngles.z * (Mathf.PI / 180.0f);//自身の向いている方向角度をラジアン化、参考元→http://ftvoid.com/blog/post/631
-        angle += h * thrusterPower * Time.deltaTime;//水平入力をもとに進行方向を変更
-        transform.rotation = Quaternion.Euler(0.0f, 0.0f, Mathf.Rad2Deg * angle);//オブジェクトに進行方向を向かせる
         direction.x = Mathf.Cos(angle);//自身の方向ベクトルを取得
         direction.y = Mathf.Sin(angle);//自身の方向ベクトルを取得
+        if (Mathf.Abs(target.x) + Mathf.Abs(target.y) >= brake)//目標地点へ近づいたらブレーキをかける
+        {
+            SetThruster(direction.normalized, target.normalized);
+            Drive(direction.normalized);
+        }
+        else
+        {
+            rb2D.AddForce(backPower * -speed * Time.deltaTime);
+        }
     }
 
     /// <summary>
-    /// ベクトル方向に直進する
+    /// 前進する
     /// </summary>
-    /// <param name="v">垂直方向の入力</param>
-    private void Drive(float v,Vector2 d)
+    /// <param name="t">進行方向ベクトル</param>
+    private void Drive(Vector2 d)
     {
-        rb2D.AddForce(v * power * d * Time.deltaTime);//directionベクトル方向に力を加える
-        rb2D.AddForce(backPower * (v * d - speed) * Time.deltaTime);//現在の進行方向と逆方向に力を加える、速度が大きいほど逆噴射も大きくなる。参考元→http://nnana-gamedev.hatenablog.com/entry/2017/09/07/012721
+        rb2D.AddForce(power * d * Time.deltaTime);//tベクトル方向に力を加える
+        rb2D.AddForce(backPower * (d - speed) * Time.deltaTime);//現在の進行方向と逆方向に力を加える、速度が大きいほど逆噴射も大きくなる。参考元→http://nnana-gamedev.hatenablog.com/entry/2017/09/07/012721
+    }
+
+    /// <summary>
+    /// 自身の方向ベクトルを目標物への方向ベクトルに重ねるように調整する
+    /// </summary>
+    /// <param name="d">正規化したdirectionベクトル</param>
+    /// <param name="t">targetベクトル</param>
+    private void SetThruster(Vector2 d, Vector2 t)
+    {
+        targetAngle = Vector3.Angle(d, t);//自身の方向ベクトルと目標物への方向ベクトルの角度差を求める
+        cross = Vector3.Cross(d, t);//自身の方向ベクトルと目標物への方向ベクトルの外積を求め、自身の方向ベクトルを基準とした場合の目標物への方向ベクトルの方角を明らかにする
+        addAngle = cross.z * thrusterPower * Time.deltaTime;
+        angle += addAngle;//両ベクトルが重なるように角度を加算減算する
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, Mathf.Rad2Deg * angle);//オブジェクトにangle方向を向かせる
     }
 }
