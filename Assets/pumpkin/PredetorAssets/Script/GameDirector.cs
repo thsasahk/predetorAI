67d5ab28fbd19c31912bf4e68878bbd6073e6d78
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-05-12 15:02:09 +0900

    機能追加
    タッチ、クリック情報をplayerControllerに渡す

--- Assets/pumpkin/PredetorAssets/Script/GameDirector.cs
+++ Assets/pumpkin/PredetorAssets/Script/GameDirector.cs
@@ -36,6 +36,10 @@ public class GameDirector : MonoBehaviour
     /// tileオブジェクト
     /// </summary>
     [SerializeField] private GameObject tile;
+    /// <summary>
+    /// playerオブジェクトのスクリプト
+    /// </summary>
+    private playerController playerCon;
 
     void Start()
     {
@@ -44,6 +48,7 @@ public class GameDirector : MonoBehaviour
         dragon = Instantiate(dragonPrefab);//dragonPrefabをインスタンスしてdragonに格納
         dragon.transform.position = dPosition;//インスタンスしたdragonオブジェクトのpositionを変更
         dragonAI = dragon.GetComponent<dragonAI>();//インスタンスしたdragonオブジェクトのスクリプトを取得
+        playerCon = player.GetComponent<playerController>();
         Instantiate(tile, Vector2.zero, Quaternion.identity);//tileオブジェクトを生成
     }
 
@@ -53,5 +58,19 @@ public class GameDirector : MonoBehaviour
         dPosition = dragon.transform.position;//現在のdragonオブジェクトの位置を記録
         pPosition = player.transform.position;//現在のplayerオブジェクトの位置を記録
         dragonAI.pPosition = pPosition;//dragonAIに現在のplayerの位置を受け渡し
+        if (Input.touchCount > 0)
+        {
+            playerCon.touch.x = Input.GetTouch(0).position.x;//スクリーン座標を記録
+            playerCon.touch.y = Input.GetTouch(0).position.y;//スクリーン座標を記録
+            playerCon.touch = Camera.main.ScreenToWorldPoint(playerCon.touch);//スクリーン座標をワールド座標に変換
+            playerCon.touch.x = Mathf.RoundToInt(playerCon.touch.x);//マス目に合わせて整数化
+            playerCon.touch.y = Mathf.RoundToInt(playerCon.touch.y);//マス目に合わせて整数化
+        }
+        if (Input.GetMouseButton(0))//開発用、ビルド時コメントアウト
+        {
+            playerCon.touch = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            playerCon.touch.x = Mathf.RoundToInt(playerCon.touch.x);//マス目に合わせて整数化
+            playerCon.touch.y = Mathf.RoundToInt(playerCon.touch.y);//マス目に合わせて整数化
+        }
     }
 }
