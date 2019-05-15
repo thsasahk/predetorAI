f3e16ebe89c9a1600710b990c5081ae01263f6d7
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-05-15 20:18:09 +0900

    機能追加
    使用する変数を用意

--- Assets/pumpkin/FlockingAssets/script/eye_flocks.cs
+++ Assets/pumpkin/FlockingAssets/script/eye_flocks.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class eye_flocks : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /// <summary>
+    /// 自身のpositionを記録
+    /// </summary>
+    private Vector2 position;
+    /// <summary>
+    /// 自身の方向ベクトル
+    /// </summary>
+    public Vector2 direction;
+    /// <summary>
+    /// 視界の長さ
+    /// </summary>
+    public float viewLength;
+    /// <summary>
+    /// 視界の角度
+    /// </summary>
+    public float viewAngle;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
