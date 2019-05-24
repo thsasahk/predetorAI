7959d5ea6c8c0b697b09e8449e2f81c9017ae9c2
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-05-24 19:28:54 +0900

    機能追加
    number変数の初期化処理を追加

--- Assets/pumpkin/FlockingAssets/script/eye_flocks.cs
+++ Assets/pumpkin/FlockingAssets/script/eye_flocks.cs
@@ -65,6 +65,11 @@ public class eye_flocks : MonoBehaviour
         angle = transform.eulerAngles.z * (Mathf.PI / 180.0f);//自身の向いている方向角度をラジアン化、参考元→http://ftvoid.com/blog/post/631
         direction.x = Mathf.Cos(angle);//自身の方向ベクトルを取得
         direction.y = Mathf.Sin(angle);//自身の方向ベクトルを取得
+    }
+
+    private void LateUpdate()
+    {
         Debug.Log(number);
+        number = 0;
     }
 }
