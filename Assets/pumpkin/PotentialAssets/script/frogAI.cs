e550592430d73899bcbe9e5df7981b8ebda471fb
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-07-23 13:28:49 +0900

    機能追加
    start時の位置をランダムに決定

--- Assets/pumpkin/PotentialAssets/script/frogAI.cs
+++ Assets/pumpkin/PotentialAssets/script/frogAI.cs
@@ -8,10 +8,23 @@ public class frogAI : MonoBehaviour
     /// slimeオブジェクトのposition
     /// </summary>
     public Vector2 slimePosition;
+    /// <summary>
+    /// start時の位置
+    /// </summary>
+    private Vector2 startPosition;
+    /// <summary>
+    /// start時の位置の最小値
+    /// </summary>
+    [SerializeField] private Vector2 minPosition;
+    /// <summary>
+    /// start時の位置の最大値
+    /// </summary>
+    [SerializeField] private Vector2 maxPosition;
 
     void Start()
     {
-        
+        startPosition.x = Random.Range(minPosition.x, maxPosition.x);//位置をランダムに決定
+        startPosition.y = Random.Range(minPosition.y, maxPosition.y);//位置をランダムに決定
     }
 
     void Update()
