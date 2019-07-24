13892b1c4fa1175efd4db596b0a6a3c9069d6ff8
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-07-24 17:35:02 +0900

    機能追加
    タッチ、クリックした位置を目標にポテンシャル関数を用いて移動する

--- Assets/pumpkin/PotentialAssets/script/frogAI.cs
+++ Assets/pumpkin/PotentialAssets/script/frogAI.cs
@@ -28,6 +28,47 @@ public class frogAI : MonoBehaviour
     /// start時の位置の最大値
     /// </summary>
     [SerializeField] private Vector2 maxPosition;
+    /// <summary>
+    /// 自身の位置
+    /// </summary>
+    private Vector2 frogPosition;
+
+    private Vector2 touchPosition;
+    /// ターゲットへの方向ベクトル
+    /// </summary>
+    private Vector2 target;
+    /// <summary>
+    /// ターゲットへの距離
+    /// </summary>
+    private float distance;
+    /// <summary>
+    /// ポテンシャル関数に使用する変数、力の合計
+    /// </summary>
+    private float U;
+    /// <summary>
+    /// ポテンシャル関数に使用する変数、引力の大きさに影響
+    /// </summary>
+    [SerializeField] private float A;
+    /// <summary>
+    /// ポテンシャル関数に使用する変数、斥力の大きさに影響
+    /// </summary>
+    [SerializeField] private float B;
+    /// <summary>
+    /// ポテンシャル関数に使用する変数、引力の大きさに影響
+    /// </summary>
+    [SerializeField] private float n;
+    /// <summary>
+    /// ポテンシャル関数に使用する変数、斥力の大きさに影響
+    /// </summary>
+    [SerializeField] private float m;
+    /// <summary>
+    /// ポテンシャル関数に使用する変数、ユニット間の距離をユニットの長さの単位で取得(ユニット何個分みたいな)
+    /// </summary>
+    private float d;
+    /// <summary>
+    /// d変数の最小値
+    /// </summary>
+    [SerializeField] private float dMin;
 
     void Start()
     {
@@ -40,6 +81,32 @@ public class frogAI : MonoBehaviour
 
     void Update()
     {
-        
+        frogPosition = transform.position;
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+        }
+        if (Input.GetMouseButton(0))//開発用、ビルド時コメントアウト
+        {
+            touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        target = (frogPosition - touchPosition).normalized;
+        distance = (frogPosition - touchPosition).magnitude;
+        rb2D.AddForce(Potencial() * target * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// ポテンシャル関数を使ってオブジェクトに与える力の大きさを計算する
+    /// </summary>
+    /// <returns></returns>
+    private float Potencial()
+    {
+        d = distance / cc2D.radius;
+        if (d <= dMin)//Uの値が急激に大きくなってしまうのを防ぐ
+        {
+            d = dMin;
+        }
+        U = -A / Mathf.Pow(d, n) + B / Mathf.Pow(d, m);//-A / Mathf.Pow(d, n)で対象からの引力、B / Mathf.Pow(d, m)で対象からの斥力を計算する
+        return U;
     }
 }
