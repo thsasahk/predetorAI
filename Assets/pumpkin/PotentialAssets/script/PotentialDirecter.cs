d101bf63b9cb2904f4480a451a2ef4f13dcf7531
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-07-29 18:24:13 +0900

    機能追加
    slime配列内のオブジェクトとflogオブジェクトの距離を一つずつ比べて最も近いものをリーダーに任命する

--- Assets/pumpkin/PotentialAssets/script/PotentialDirecter.cs
+++ Assets/pumpkin/PotentialAssets/script/PotentialDirecter.cs
@@ -52,6 +52,22 @@ public class PotentialDirecter : MonoBehaviour
     /// capsuleオブジェクトの配置の最大値
     /// </summary>
     [SerializeField] private Vector2 maxPosition;
+    /// <summary>
+    /// リーダーを用意するかどうかを決定する
+    /// </summary>
+    [SerializeField] private bool leader;
+    /// <summary>
+    /// slimeオブジェクトからfrogオブジェクトへの距離
+    /// </summary>
+    private float distance;
+    /// <summary>
+    /// もっとも近いslimeオブジェクトからfrogオブジェクトへの距離
+    /// </summary>
+    private float minDis;
+    /// <summary>
+    /// リーダーオブジェクトの要素番号
+    /// </summary>
+    private int leaderNumber;
 
     void Start()
     {
@@ -77,6 +93,21 @@ public class PotentialDirecter : MonoBehaviour
 
     void Update()
     {
+        if (leader)
+        {
+            distance = 0;//初期化
+            for (int n = 0; n < sNumber; n++)//slime配列内のオブジェクトとflogオブジェクトの距離を一つずつ比べる
+            {
+                slimeAI[n].isLeader = false;//初期化
+                distance = (slime[n].transform.position - frog.transform.position).magnitude;
+                if (distance < minDis || distance == 0)
+                {
+                    minDis = distance;
+                    leaderNumber = n;
+                }
+            }
+            slimeAI[leaderNumber].isLeader = true;
+        }
         for (int n = 0; n < sNumber; n++)
         {
             slimeAI[n].frogPosition = frog.transform.position;//flogオブジェクトのpositionを受け渡す
