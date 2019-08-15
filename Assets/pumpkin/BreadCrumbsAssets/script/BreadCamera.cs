6197068a400427f042d9bb773f2632fa6cbe134f
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-08-15 13:07:19 +0900

    機能追加
    start時にdirecterを生成するBreadCameraスクリプトを作成、MainCameraにアタッチ

---
+++ Assets/pumpkin/BreadCrumbsAssets/script/BreadCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreadCamera : MonoBehaviour
+{
+    /// <summary>
+    /// GameDirecter
+    /// </summary>
+    [SerializeField] private GameObject directer;
+
+    void Start()
+    {
+        Instantiate(directer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
