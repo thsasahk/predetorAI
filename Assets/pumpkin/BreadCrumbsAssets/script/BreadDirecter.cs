2773c30a87a60e6037addfae2caede24d51ea896
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-08-15 12:52:53 +0900

    機能追加
    playerPrefabオブジェクト、enemyPrefabオブジェクト、stonePrefabオブジェクトを生成

--- Assets/pumpkin/BreadCrumbsAssets/script/BreadDirecter.cs
+++ Assets/pumpkin/BreadCrumbsAssets/script/BreadDirecter.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class BreadDirecter : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /// <summary>
+    /// PlayerPrefab
+    /// </summary>
+    [SerializeField] private GameObject playerPrefab;
+    /// <summary>
+    /// 生成したPlayerPrefab
+    /// </summary>
+    private GameObject player;
+    /// <summary>
+    /// EnemyPrefab
+    /// </summary>
+    [SerializeField] private GameObject enemyPrefab;
+    /// <summary>
+    /// 生成したEnemyPrefab
+    /// </summary>
+    private GameObject enemy;
+    /// <summary>
+    /// StonePrefab
+    /// </summary>
+    [SerializeField] private GameObject stonePrefab;
+    /// <summary>
+    /// 生成したStonePrefab
+    /// </summary>
+    private GameObject stone;
+
     void Start()
     {
-        
+        player = Instantiate(playerPrefab);
+        enemy = Instantiate(enemyPrefab);
+        stone = Instantiate(stonePrefab);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
