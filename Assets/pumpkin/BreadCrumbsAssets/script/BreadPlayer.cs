c3d907c465867b4f1124ab0146aec4a63ca309f2
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-08-25 10:24:37 +0900

    機能追加
    自身の位置と移動目標とを結ぶベクトルをdelta変数に格納

--- Assets/pumpkin/BreadCrumbsAssets/script/BreadPlayer.cs
+++ Assets/pumpkin/BreadCrumbsAssets/script/BreadPlayer.cs
@@ -20,16 +20,36 @@ public class BreadPlayer : MonoBehaviour
     /// 移動速度
     /// </summary>
     [SerializeField] private float step;
+    /// <summary>
+    /// 移動目標
+    /// </summary>
+    private Vector2 target;
+    /// <summary>
+    /// 自身からtargetへのベクトル
+    /// </summary>
+    private Vector2 delta;
 
     void Start()
     {
         nextCell = gameObject.transform.position;//初期化
+        target = gameObject.transform.position;//初期化
     }
 
     void Update()
     {
         current = gameObject.transform.position;//変数を更新
         nextCell = touch;//仮の記述
+        if (target != touch && current == nextCell)
+        {
+            target = touch;//移動目標を更新
+            delta = target - current;
+            SetPath(delta);
+        }
         transform.position = Vector3.MoveTowards(current, nextCell, step * Time.deltaTime);//目標地点へ移動
+    }
+
+    private void SetPath(Vector2 d)
+    {
+
     }
 }
