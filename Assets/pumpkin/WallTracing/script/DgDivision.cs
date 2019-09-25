83e237a087727ed85ce6f6feea55525432a675e1
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-09-25 19:13:40 +0900

    機能追加
    DgDivisionを追加

---
+++ Assets/pumpkin/WallTracing/script/DgDivision.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//　ダンジョン区画情報
+// 参照元:https://qiita.com/2dgames_jp/items/00ee2ad52914753bfbb7
+public class DgDivision
+{
+    /// <summary>
+    /// 矩形管理
+    /// </summary>
+    public class DgRect
+    {
+        /// <summary>
+        /// 左
+        /// </summary>
+        public int Left = 0;
+        /// <summary>
+        /// 上
+        /// </summary>
+        public int Top = 0;
+        /// <summary>
+        /// 右
+        /// </summary>
+        public int Right = 0;
+        /// <summary>
+        /// 下
+        /// </summary>
+        public int Bottom = 0;
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public DgRect()
+        {
+            // 特に何もしない
+        }
+        /// <summary>
+        /// 高さ
+        /// </summary>
+        public int Height
+        {
+            get { return Bottom - Top; }
+        }
+        /// <summary>
+        /// 幅
+        /// </summary>
+        public int Width
+        {
+            get { return Right - Left; }
+        }
+        /// <summary>
+        /// 面積 (幅 x 高さ)
+        /// </summary>
+        public int Measure
+        {
+            get { return Width * Height; }
+        }
+
+        /// <summary>
+        /// 値をまとめて設定する
+        /// </summary>
+        /// <param name="left">左</param>
+        /// <param name="top">上</param>
+        /// <param name="right">右</param>
+        /// <param name="bottom">左</param>
+        public void Set(int left, int top, int right, int bottom)
+        {
+            Left = left;
+            Top = top;
+            Right = right;
+            Bottom = bottom;
+        }
+
+        /// <summary>
+        /// 矩形情報をコピーする
+        /// </summary>
+        /// <param name="rect">コピー元の矩形情報</param>
+        public void Copy(DgRect rect)
+        {
+            Left = rect.Left;
+            Top = rect.Top;
+            Right = rect.Right;
+            Bottom = rect.Bottom;
+        }
+
+        /// <summary>
+        /// デバッグ出力
+        /// </summary>
+        public void Dump()
+        {
+            Debug.Log(string.Format("<Rect l,t,r,b = {0},{1},{2},{3}> w,h = {4},{5}",
+                Left, Top, Right, Bottom, Width, Height));
+        }
+    }
+
+    /// <summary>
+    /// 外周の矩形情報
+    /// </summary>
+    public DgRect Outer;
+    /// <summary>
+    /// 区画内に作ったルーム情報
+    /// </summary>
+    public DgRect Room;
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public DgDivision()
+    {
+        Outer = new DgRect();
+        Room = new DgRect();
+    }
+    /// <summary>
+    /// デバッグ出力
+    /// </summary>
+    public void Dump()
+    {
+        Outer.Dump();
+        Room.Dump();
+    }
+}
