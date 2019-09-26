7b6fb1c6fee81b731b6de606d48d5618793c72ad
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-09-26 11:52:46 +0900

    機能追加
    DgGeneratorを追加

---
+++ Assets/pumpkin/WallTracing/script/DgGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ダンジョンの自動生成モジュール
+/// </summary>
+// 参照元:https://qiita.com/2dgames_jp/items/00ee2ad52914753bfbb7
+public class DgGenerator : MonoBehaviour
+{
+    /// <summary>
+    /// 2次元配列情報
+    /// </summary>
+    Layer2D _layer = null;
+    /// <summary>
+    /// 区画リスト
+    /// </summary>
+    List<DgDivision> _divList = null;
+}
