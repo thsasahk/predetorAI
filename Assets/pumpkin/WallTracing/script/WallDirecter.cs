31c8553ca778e5da65ada1d6c73740d4d158d6ae
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-10-18 00:35:28 +0900

    機能追加
    WallDirecterを追加

---
+++ Assets/pumpkin/WallTracing/script/WallDirecter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDirecter : MonoBehaviour
+{
+    [SerializeField] private GameObject DgGenerator;
+    
+    void Start()
+    {
+        Instantiate(DgGenerator);
+    }
+
+    void Update()
+    {
+        
+    }
+}
