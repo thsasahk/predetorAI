50bce0d8a6ae631aa8f14f264b5817e1cdcb45f9
thsasahk <45602467+thsasahk@users.noreply.github.com>
2019-10-20 08:18:39 +0900

    機能追加
    コメント追加

--- Assets/pumpkin/WallTracing/script/wall.cs
+++ Assets/pumpkin/WallTracing/script/wall.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class wall : MonoBehaviour
 {
+    /// <summary>
+    /// マス目のサイズ
+    /// </summary>
     public Vector2 ratio;
+
     void Start()
     {
         gameObject.GetComponent<RectTransform>().sizeDelta =
             new Vector2(gameObject.GetComponent<RectTransform>().sizeDelta.x * ratio.x,
-            gameObject.GetComponent<RectTransform>().sizeDelta.y * ratio.y);
+            gameObject.GetComponent<RectTransform>().sizeDelta.y * ratio.y);//マス目のサイズに合わせて自身のサイズを変更する
     }
 
     void Update()
